8bd0392e7b474a3b304711a1e61519cc0e176200
MainWane <ulrikwandall@outlook.com>
2024-09-27 10:03:41 +0200

    Ny Mappe og fil til kode

---
+++ Milepæl 1/Uge 36/Kode.cpp
@@ -0,0 +1,15 @@
+
+//Ulrik
+// Mit første C++ program
+#include <iostream>
+
+
+int main()
+{
+int a=5; // værdi = 5
+int b(2); // værdi = 2
+int result; // ukendt værdi
+a = a + 3;
+result = a - b; // resultat udregnes
+std::cout << result; // print resultatet
+}
