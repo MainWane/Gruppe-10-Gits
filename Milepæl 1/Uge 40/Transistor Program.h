391df7472368632f762312d06ec5acaecb4c44e5
MainWane <ulrikwandall@outlook.com>
2024-10-04 20:26:13 +0200

    Update Transistor Program.h

--- Milepæl 1/Uge 40/Transistor Program.h
+++ Milepæl 1/Uge 40/Transistor Program.h
@@ -1,4 +1,17 @@
 //Ulrik
+//LED blink program 
+
+#include <Arduino.h>
+
+
+#define LED 12 //Angivet som D12 på ESP32’eren
+
+
+void setup () {
+// setup code, kør en gang
+Serial.begin(115200);
+pinMode(LED, OUTPUT);
+}
 
 
 void loop() {
